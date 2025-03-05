962490d52bf88e5f4c8cef7e0c7e45bf63dac91c
Ebisuzawa Kurumi <164317648+shovel-kun@users.noreply.github.com>
2025-03-06 05:31:37 +0800

    Update BaseMPVView.kt

--- app/src/main/java/is/xyz/mpv/BaseMPVView.kt
+++ app/src/main/java/is/xyz/mpv/BaseMPVView.kt
@@ -8,7 +8,7 @@ import android.view.SurfaceView
 
 // Contains only the essential code needed to get a picture on the screen
 
-abstract class BaseMPVView(context: Context, attrs: AttributeSet) : SurfaceView(context, attrs), SurfaceHolder.Callback {
+abstract class BaseMPVView(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
     /**
      * Initialize libmpv.
      *
